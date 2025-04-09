4f9250e065684bd7311aae1c5e1389ed470b0a54
Akio Hamamura <73171585+AkioHamamura@users.noreply.github.com>
2025-04-08 22:08:02 -0500

    New Project Commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen space-y-20">
       <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl">
-        Vite, React, Shadcn-ui minimal starter
+        Vitee, React, Shadcn-ui minimal starter
       </h1>
       <a
         href="https://github.com/moinulmoin/vite-react-tailwind-starter"
